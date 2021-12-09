96eb757330dc71eeac27a7e1bdaa00479c7598fa
Yuqifan1117 <1138414330@qq.com>
2021-12-09 21:23:46 +0800

    Get dataset

---
+++ test.py
@@ -0,0 +1,13 @@
+import pickle
+
+
+def unpickle(file):
+    
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+
+
+if __name__ == '__main__':
+    dict = unpickle('cifar-10-batches-py\data_batch_1')
+    print(len(dict))
